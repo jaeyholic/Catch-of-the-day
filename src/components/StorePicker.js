595a705c7705df87562a8529ca9eb59032104cac
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-02-05 15:44:27 +0000

    Adding CSS
    Boostraping the project with style.css and creating the homepage form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class StorePicker extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Please Enter a store</h2>
+          <input type="text" placeholder="Store Name" />
+          <button type="submit">Visit Store →</button>
+        </form>
+      </React.Fragment>
+    );
   }
 }
