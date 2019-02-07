99920317bd59fa571cc70e793cfa4165bc173f45
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-02-07 14:21:44 +0000

    Order Component
    Worked on the order component to receive orders when added to the cart

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import { formatPrice } from "../helpers";
 
 export default class Order extends Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available.
+        </li>
+      );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
   render() {
+    const orderId = Object.keys(this.props.order);
+    const total = orderId.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
     return (
       <div className="order-wrap">
         <h2>Order</h2>
+        <ul className="order">{orderId.map(this.renderOrder)}</ul>
+        <div className="total">
+          <strong>Total: {formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
